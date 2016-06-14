4f90557651c36ac4f40945e41b2444847e4933e8
sunshuhan <526429929@qq.com>
2016-06-14 22:28:19 +0800

    time t

--- citypipe/WebContent/TTSWebGLApp/javascript/TTSObject.js
+++ citypipe/WebContent/TTSWebGLApp/javascript/TTSObject.js
@@ -1,11 +1,7 @@
 /*
 物体即网格(mesh)，由geometry和material定义
-
-*/
-//var sphere = new THREE.SphereGeometry( 1, 30, 30 );
-//var material = new THREE.MeshLambertMaterial( { color:0x333333, side: THREE.DoubleSide } );
-//var	pmesh = new THREE.Mesh( sphere, material );
 //att1,2,3,4 are extra properties for descripbing object information,such as radius property for sphere object
+*/
 function TTSObject(id,pipetype,tx,ty,tz,sx,sy,sz,rx,ry,rz,c,att1,att2,att3,att4){
 	this.id = id;
 	this.type = pipetype;
@@ -22,48 +18,55 @@ function TTSObject(id,pipetype,tx,ty,tz,sx,sy,sz,rx,ry,rz,c,att1,att2,att3,att4)
 	this.att1 = att1;
 	this.mesh=null;	
 }
-/*
-TTSObject.prototype.refresh=function()
-{
-
-	
-}
-*/
 TTSObject.prototype.removeFromScene = function()
 {
+	if(this.mesh !== null)
 	scene.remove(this.mesh);
 }
-TTSObject.prototype.addToScene = function()
+TTSObject.prototype.addToScene = function(t)
 {	
-	rx=this.rotation_x;
-	ry=this.rotation_y;
-	rz=this.rotation_z;
-	tx=this.translation_x;
-	ty=this.translation_y;
-	tz=this.translation_z;
-	sx=this.scale_x;
-	sy=this.scale_y;
-	sz=this.scale_z;
-	att1=this.att1;
-	c=this.color;
-	var mesh;
+	t = t||0;
+	rx = eval(this.rotation_x);
+	ry = eval(this.rotation_y);
+	rz = eval(this.rotation_z);
+	tx = eval(this.translation_x);
+	ty = eval(this.translation_y);
+	tz = eval(this.translation_z);
+	sx = eval(this.scale_x);
+	sy = eval(this.scale_y);
+	sz = eval(this.scale_z);
 	if(this.mesh === null)
-	if(this.type=='sphere')
-	{			
-		var sphere = new THREE.SphereGeometry( att1, 30, 30 );
-		var material = new THREE.MeshLambertMaterial( { color: c, side: THREE.DoubleSide } );
-		mesh = new THREE.Mesh( sphere, material );
-		mesh.rotation.x=rx;
-		mesh.rotation.y=ry;
-		mesh.rotation.z=rz;
-		mesh.position.x=tx; 
-		mesh.position.y=ty;
-		mesh.position.z=tz;
-		mesh.scale.x=sx;
-		mesh.scale.y=sy;
-		mesh.scale.z=sz;
-		mesh.objname='sphere';	
-		this.mesh=mesh;
-		scene.add(this.mesh);
+	{
+		//add to scene according to the object type
+		if(this.type=='sphere')
+		{			
+			var sphere = new THREE.SphereGeometry( this.att1, 30, 30 );
+			var material = new THREE.MeshLambertMaterial( { color: this.color, side: THREE.DoubleSide } );
+			var mesh = new THREE.Mesh( sphere, material );
+			mesh.rotation.x=rx;
+			mesh.rotation.y=ry;
+			mesh.rotation.z=rz;
+			mesh.position.x=tx; 
+			mesh.position.y=ty;
+			mesh.position.z=tz;
+			mesh.scale.x=sx;
+			mesh.scale.y=sy;
+			mesh.scale.z=sz;
+			mesh.objname='sphere';	
+			this.mesh=mesh;
+			scene.add(this.mesh);
+		}
+	}
+	else if(this.mesh !== null){
+		//refresh the mesh position....
+		this.mesh.rotation.x=rx;
+		this.mesh.rotation.y=ry;
+		this.mesh.rotation.z=rz;
+		this.mesh.position.x=tx; 
+		this.mesh.position.y=ty;
+		this.mesh.position.z=tz;
+		this.mesh.scale.x=sx;
+		this.mesh.scale.y=sy;
+		this.mesh.scale.z=sz;	
 	}
 }
